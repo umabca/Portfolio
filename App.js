e6aa1b3b4add0199e592c2bbd918435d177bfe85
umabca <160339973+umabca@users.noreply.github.com>
2026-02-09 23:13:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Skills />
+      <Projects />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ data.js
@@ -0,0 +1,23 @@
+export const videos = [
+  {
+    id: "w7ejDZ8SWv8",
+    title: "React JS Crash Course",
+    channel: "CodeWithUma",
+    views: "1.2M",
+    thumbnail: "https://i.ytimg.com/vi/w7ejDZ8SWv8/hqdefault.jpg"
+  },
+  {
+    id: "PkZNo7MFNFg",
+    title: "JavaScript Full Tutorial",
+    channel: "FreeCodeCamp",
+    views: "3.5M",
+    thumbnail: "https://i.ytimg.com/vi/PkZNo7MFNFg/hqdefault.jpg"
+  },
+  {
+    id: "mU6anWqZJcc",
+    title: "HTML & CSS Full Course",
+    channel: "Traversy Media",
+    views: "900K",
+    thumbnail: "https://i.ytimg.com/vi/mU6anWqZJcc/hqdefault.jpg"
+  }
+];
